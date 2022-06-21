08aae5e56ee776ae0840b7c7dcf5f2505b091c9c
FranciscoM <francisco.marquez@meaningfulteam.com>
2022-06-20 22:08:48 -0300

    Agregamos final del url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://FranciscoMeaningful.github.io/',
+  base: 'https://FranciscoMeaningful.github.io/react-vite-actions',
   plugins: [react()]
 })
